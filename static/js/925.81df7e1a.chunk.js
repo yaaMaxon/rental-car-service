194fdcfe959d9f360ad54bddfece73d9799a46af
yaaMaxon <127898896+yaaMaxon@users.noreply.github.com>
2024-02-16 18:15:28 +0000

    Deploying to gh-pages from @ yaaMaxon/rental-car-service@b840c71bbf5dbfc68c7d143792853807a95325fb 🚀

---
+++ static/js/925.81df7e1a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[925],{925:function(e,t,r){r.r(t);r(791);var a=r(184);t.default=function(){return(0,a.jsx)("div",{})}}}]);
+//# sourceMappingURL=925.81df7e1a.chunk.js.map
